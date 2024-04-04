dbbf77c78b5988efe25c0b705348c21913946de9
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-04-03 23:46:19 -0600

    5 & 6

---
+++ 6 - 189. Rotate Array.py
@@ -0,0 +1,9 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            temp=nums[-1]
+            nums.pop()
+            nums.insert(0,temp)
