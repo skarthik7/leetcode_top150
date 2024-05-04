636892e19cd7290d5ec1c4667cdf6cfdc6525766
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-03 23:45:15 -0600

    Update 45 - 202. Happy Number.py

---
+++ Hashmap/45 - 202. Happy Number.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        def get_next(number):
+            total_sum = 0
+            while number > 0:
+                number, digit = divmod(number, 10)
+                total_sum += digit ** 2
+            return total_sum
+
+        slow = n
+        fast = get_next(n)
+        while fast != 1 and slow != fast:
+            slow = get_next(slow)
+            fast = get_next(get_next(fast))
+
+        return fast == 1
