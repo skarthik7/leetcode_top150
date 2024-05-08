57ba7c53d7bb13b731b301901138c0df08ea5781
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-07 18:19:43 -0600

    Create 67 - 146. LRU Cache.py

---
+++ Linked List/67 - 146. LRU Cache.py
@@ -0,0 +1,54 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = {}
+        self.capacity = capacity
+        self.dummy_head = Node(0, 0)
+        self.dummy_tail = Node(0, 0)
+        self.dummy_head.next = self.dummy_tail
+        self.dummy_tail.prev = self.dummy_head
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add(node)
+            return node.value
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add(node)
+        self.cache[key] = node
+        if len(self.cache) > self.capacity:
+            node = self.dummy_head.next
+            self._remove(node)
+            del self.cache[node.key]
+
+    def _remove(self, node):
+        prev = node.prev
+        next = node.next
+        prev.next = next
+        next.prev = prev
+
+    def _add(self, node):
+        prev = self.dummy_tail.prev
+        prev.next = node
+        self.dummy_tail.prev = node
+        node.prev = prev
+        node.next = self.dummy_tail
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
