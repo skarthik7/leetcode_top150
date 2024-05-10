c7a121eef50a673bd9179d7a37dc3f60c57bb059
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-09 22:08:33 -0600

    Update 39 - 383. Ransom Note.py
    used hashmap

--- Hashmap/39 - 383. Ransom Note.py
+++ Hashmap/39 - 383. Ransom Note.py
@@ -1,8 +1,17 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for letter in ransomNote:
-            if letter in magazine:
-                magazine = magazine.replace(letter,"",1)
+        hashMap = {}
+        for char in magazine:
+            if char in hashMap:
+                hashMap[char] += 1
+            else:
+                hashMap[char] = 1
+        for char in ransomNote:
+            if char in hashMap:
+                if hashMap[char] == 0:
+                    del hashMap[char]
+                    return False
+                hashMap[char] -= 1
             else:
                 return False
         return True
