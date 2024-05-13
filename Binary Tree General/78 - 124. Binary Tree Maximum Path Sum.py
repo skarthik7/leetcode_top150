65d18b438d8a70468291555a1f0c8f268843b167
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-12 18:23:48 -0600

    Create 78 - 124. Binary Tree Maximum Path Sum.py

---
+++ Binary Tree General/78 - 124. Binary Tree Maximum Path Sum.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        max_sum = float('-inf')
+
+        def max_gain(node):
+            nonlocal max_sum
+            if not node:
+                return 0
+            left_gain = max(max_gain(node.left), 0)
+            right_gain = max(max_gain(node.right), 0)
+            max_sum = max(max_sum, node.val + left_gain + right_gain)
+            return node.val + max(left_gain, right_gain)
+
+        max_gain(root)
+        return max_sum
