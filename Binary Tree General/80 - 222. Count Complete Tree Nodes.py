dddbc8ec985de53979907d76654822fbb8649c74
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-13 22:21:47 -0600

    push

---
+++ Binary Tree General/80 - 222. Count Complete Tree Nodes.py
@@ -0,0 +1,10 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+        # TO DO: Implement the solution
+        pass
