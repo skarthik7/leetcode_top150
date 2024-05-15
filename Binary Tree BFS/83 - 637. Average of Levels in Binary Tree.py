72e4d0b8e50be38f4f9799427e4d0ca7b61da6d8
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-14 22:57:39 -0600

    83 - 637 edit

--- Binary Tree BFS/83 - 637. Average of Levels in Binary Tree.py
+++ Binary Tree BFS/83 - 637. Average of Levels in Binary Tree.py
@@ -1,5 +1,9 @@
-from collections import deque
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
         queue = deque([root])
