13206ae47d6c449570073415b11f713d339d1ee9
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-17 17:19:30 -0600

    Update 94 - 210. Course Schedule II.py

--- Graph General/94 - 210. Course Schedule II.py
+++ Graph General/94 - 210. Course Schedule II.py
@@ -1,27 +1,17 @@
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = [[] for _ in range(numCourses)]
-        visited = [0 for _ in range(numCourses)]
+        indegree = [0 for _ in range(numCourses)]
         for x, y in prerequisites:
-            graph[x].append(y)
-        
-        def dfs(i):
-            if visited[i] == -1:
-                return True
-            if visited[i] == 1:
-                return False
-            visited[i] = -1
-            for j in graph[i]:
-                if dfs(j):
-                    return True
-            visited[i] = 1
-            return False
-        
+            graph[y].append(x)
+            indegree[x] += 1
+        stack = [i for i in range(numCourses) if indegree[i] == 0]
         order = []
-        for i in range(numCourses):
-            if dfs(i):
-                return []
-            if visited[i] == 0:
-                visited[i] = 1
-                order.append(i)
-        return order
+        while stack:
+            node = stack.pop()
+            order.append(node)
+            for i in graph[node]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    stack.append(i)
+        return order if len(order) == numCourses else []
