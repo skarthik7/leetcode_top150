a8ccd1938e73a02c6826b7fa43407a0e3c8a259d
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-16 22:34:07 -0600

    89 - 200.py

---
+++ Binary Tree BFS/89 - 200. Number of Islands.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def dfs(grid, row, col):
+            if row < 0 or col < 0 or row >= len(grid) or col >= len(grid[0]) or grid[row][col] != '1':
+                return
+            grid[row][col] = '0'  #visited
+            dfs(grid, row + 1, col)
+            dfs(grid, row - 1, col)
+            dfs(grid, row, col + 1)
+            dfs(grid, row, col - 1)
+
+        islands = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '1':
+                    dfs(grid, row, col)
+                    islands += 1
+
+        return islands
