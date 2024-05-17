e45ed5c89bba25092f0998de3981493856392318
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-16 22:36:05 -0600

    Create 90 - 130. Surrounded Regions.py

---
+++ Graph General/90 - 130. Surrounded Regions.py
@@ -0,0 +1,32 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        if not board or not board[0]:
+            return
+        m, n = len(board), len(board[0])
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= m or j >= n or board[i][j] != 'O':
+                return
+            board[i][j] = 'D'
+            dfs(i - 1, j)
+            dfs(i + 1, j)
+            dfs(i, j - 1)
+            dfs(i, j + 1)
+
+        for i in range(m):
+            dfs(i, 0)
+            dfs(i, n - 1)
+
+        for j in range(n):
+            dfs(0, j)
+            dfs(m - 1, j)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'O':
+                    board[i][j] = 'X'
+                elif board[i][j] == 'D':
+                    board[i][j] = 'O'
