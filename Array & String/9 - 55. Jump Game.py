2639c9dd4eeb4af3b967ecad91adb5e4f92ac7ef
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-19 16:26:44 -0600

    Jump Game

--- Array & String/9 - 55. Jump Game.py
+++ Array & String/9 - 55. Jump Game.py
@@ -1,7 +1,7 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         reach = 0
-        for i in range(len(nums) - 1):
+        for i in range(len(nums)):
             if i > reach:
                 return False
             reach = max(reach, i + nums[i])
