5caf72664f0ce6f30c928e71ee63e5664b97d382
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-05-25 23:37:45 -0600

    edits

--- Backtracking/102 - 77. Combinations.py
+++ Backtracking/102 - 77. Combinations.py
@@ -1,14 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
-        self._backtrack(1, [], n, k, result)
+        self.backtrack(1, [], n, k, result)
         return result
 
-    def _backtrack(self, start, curr, n, k, result):
+    def backtrack(self, start, curr, n, k, result):
         if len(curr) == k:
             result.append(curr[:])
             return
         for i in range(start, n + 1):
             curr.append(i)
-            self._backtrack(i + 1, curr, n, k, result)
+            self.backtrack(i + 1, curr, n, k, result)
             curr.pop()
