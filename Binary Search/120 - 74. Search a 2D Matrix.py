b3735bb26f3b54344a02fa736bbe48a63f0106cf
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-03 22:51:49 +0300

    Update 120 - 74. Search a 2D Matrix.py

---
+++ Binary Search/120 - 74. Search a 2D Matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            row, col = divmod(mid, n)
+            if matrix[row][col] == target:
+                return True
+            elif matrix[row][col] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return False
