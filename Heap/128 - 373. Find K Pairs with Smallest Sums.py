2536db7ec8a5bd35a3c2733af986083a44f7ac3b
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-06 11:41:38 +0300

    Create 128 - 373. Find K Pairs with Smallest Sums.py

---
+++ Heap/128 - 373. Find K Pairs with Smallest Sums.py
@@ -0,0 +1,19 @@
+import heapq
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2:
+            return []
+        min_heap = [(nums1[0] + nums2[0], 0, 0)]
+        visited = set((0, 0))
+        result = []
+        while min_heap and len(result) < k:
+            _, i, j = heapq.heappop(min_heap)
+            result.append([nums1[i], nums2[j]])
+            if i + 1 < len(nums1) and (i + 1, j) not in visited:
+                heapq.heappush(min_heap, (nums1[i + 1] + nums2[j], i + 1, j))
+                visited.add((i + 1, j))
+            if j + 1 < len(nums2) and (i, j + 1) not in visited:
+                heapq.heappush(min_heap, (nums1[i] + nums2[j + 1], i, j + 1))
+                visited.add((i, j + 1))
+        return result
