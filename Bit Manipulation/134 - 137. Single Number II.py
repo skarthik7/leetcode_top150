25c8172440c70e324e053c5f69a9a4baae72fc63
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-09 12:19:48 +0300

    Update 134 - 137. Single Number II.py

--- Bit Manipulation/134 - 137. Single Number II.py
+++ Bit Manipulation/134 - 137. Single Number II.py
@@ -1,3 +1,10 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        pass
+        ones = twos = 0
+        for num in nums:
+            twos |= ones & num
+            ones ^= num
+            common_bits = ones & twos
+            ones &= ~common_bits
+            twos &= ~common_bits
+        return ones
