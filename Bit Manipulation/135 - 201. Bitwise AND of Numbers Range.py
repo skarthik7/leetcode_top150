c4af29a46c977a78abe1a84b88270867448d6d82
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-09 12:22:02 +0300

    Update 135 - 201. Bitwise AND of Numbers Range.py

--- Bit Manipulation/135 - 201. Bitwise AND of Numbers Range.py
+++ Bit Manipulation/135 - 201. Bitwise AND of Numbers Range.py
@@ -1,3 +1,9 @@
 class Solution:
-    def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        pass
+        def rangeBitwiseAnd(self, left: int, right: int) -> int:
+            shift = 0
+            while left != right:
+                left >>= 1
+                right >>= 1
+                shift += 1
+            return left << shift
+# Time complexity: O(1)
