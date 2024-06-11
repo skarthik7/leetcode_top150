4f3c7739f8e275237267cba220cdf0f81bb92ae1
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-11 18:52:15 +0300

    Update 140 - 50. Pow(x, n).py

--- Math/140 - 50. Pow(x, n).py
+++ Math/140 - 50. Pow(x, n).py
@@ -1,1 +1,16 @@
 # 50. Pow(x, n)
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n == 0:
+            return 1.0
+        if n < 0:
+            x = 1 / x
+            n = -n
+        result = 1
+        while n:
+            if n % 2:
+                result *= x
+            x *= x
+            n //= 2
+        return result
+# Time: O(log(n))
