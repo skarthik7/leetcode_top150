aa682f68192a151341b3a9bb454e02bd0342e06f
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-15 17:20:56 +0300

    Update 144 - 139. Word Break.py

--- 1D DP/144 - 139. Word Break.py
+++ 1D DP/144 - 139. Word Break.py
@@ -1,3 +1,11 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        pass
+        dp = [False] * (len(s) + 1)
+        dp[0] = True
+        wordSet = set(wordDict)
+        for i in range(1, len(s) + 1):
+            for j in range(i):
+                if dp[j] and s[j:i] in wordSet:
+                    dp[i] = True
+                    break
+        return dp[len(s)]
