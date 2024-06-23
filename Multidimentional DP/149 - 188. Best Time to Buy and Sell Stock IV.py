6ba19d44f5f071e8fcf7cf5e7fc86387dd3c9a37
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-23 16:03:46 +0300

    Update 149 - 188. Best Time to Buy and Sell Stock IV.py

---
+++ Multidimentional DP/149 - 188. Best Time to Buy and Sell Stock IV.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not prices or k == 0:
+            return 0
+
+        n = len(prices)
+        if k >= n // 2:
+            profit = 0
+            for i in range(1, len(prices)):
+                profit += max(prices[i] - prices[i - 1], 0)
+            return profit 
+        dp = [[0] * n for _ in range(k + 1)]
+
+        for i in range(1, k + 1):
+            max_diff = -prices[0]  
+            for j in range(1, n):
+                dp[i][j] = max(dp[i][j-1], prices[j] + max_diff) 
+                max_diff = max(max_diff, dp[i-1][j] - prices[j]) 
+
+        return dp[k][n-1]
