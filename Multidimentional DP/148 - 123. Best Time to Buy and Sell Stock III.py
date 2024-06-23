68a30ba30e27e1373d4e0cd5a79dffe5f39a9df4
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-23 16:00:24 +0300

    Update 148 - 123. Best Time to Buy and Sell Stock III.py

--- Multidimentional DP/148 - 123. Best Time to Buy and Sell Stock III.py
+++ Multidimentional DP/148 - 123. Best Time to Buy and Sell Stock III.py
@@ -1,4 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # TODO: Implement the solution here
-        pass
+        if not prices:
+            return 0
+        
+        buy1= float('-inf')
+        sell1 = 0
+        buy2 = float('-inf')
+        sell2 = 0
+        
+        for price in prices:
+            sell2 = max(sell2, buy2 + price)
+            buy2 = max(buy2, sell1 - price)
+            sell1 = max(sell1, buy1 + price)
+            buy1 = max(buy1, -price)
+
+        return sell2
