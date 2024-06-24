2b2727220a589c25017980389d13d68cd1323f68
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-24 10:53:47 +0300

    Update 80 - 222. Count Complete Tree Nodes.py

--- Binary Tree General/80 - 222. Count Complete Tree Nodes.py
+++ Binary Tree General/80 - 222. Count Complete Tree Nodes.py
@@ -6,5 +6,18 @@
 #         self.right = right
 class Solution:
     def countNodes(self, root: Optional[TreeNode]) -> int:
-        # TO DO: Implement the solution
-        pass
+        if not root:
+            return 0
+        left_depth = self.getDepth(root, True)
+        right_depth = self.getDepth(root, False)
+        
+        if left_depth == right_depth:
+            return (1 << left_depth) - 1
+        else:
+            return 1 + self.countNodes(root.left) + self.countNodes(root.right)
+    def getDepth(self, node: Optional[TreeNode], isLeft: bool) -> int:
+        depth = 0
+        while node:
+            depth += 1
+            node = node.left if isLeft else node.right
+        return depth
