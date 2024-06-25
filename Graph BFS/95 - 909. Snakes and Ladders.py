284a88516fce70991536b6ddb5a6f68e67e0ef22
Sriram Karthik Akella <sriramka@ualberta.ca>
2024-06-25 17:47:47 +0530

    Update 95 - 909. Snakes and Ladders.py

---
+++ Graph BFS/95 - 909. Snakes and Ladders.py
@@ -0,0 +1,23 @@
+def snakesAndLadders(board):
+    n = len(board)
+    def get(s):
+        # Convert the square s to board coordinates (r, c)
+        quot, rem = divmod(s-1, n)
+        row = n - 1 - quot
+        col = rem if row % 2 != n % 2 else n - 1 - rem
+        return row, col
+
+    visited = set()
+    queue = [(1, 0)]  # (square, moves)
+    while queue:
+        s, moves = queue.pop(0)
+        if s == n*n:
+            return moves
+        for next_s in range(s+1, min(s+6, n*n) + 1):
+            r, c = get(next_s)
+            if board[r][c] != -1:
+                next_s = board[r][c]
+            if next_s not in visited:
+                visited.add(next_s)
+                queue.append((next_s, moves + 1))
+    return -1
